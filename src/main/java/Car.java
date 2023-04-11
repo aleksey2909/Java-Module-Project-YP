fb5c3ce685dd8d63a61bbcee8437493e3624f03d
Igor Avdonin <thathorizon@gmail.com>
2023-04-11 10:48:00 +0300

    Init commit

---
+++ src/main/java/Car.java
@@ -0,0 +1,9 @@
+public class Car {
+    String modelCar;
+    int speed;
+
+    public Car(String modelCar, int speed) {
+        this.modelCar = modelCar;
+        this.speed = speed;
+    }
+}
---
+++ src/main/java/Race.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Race {
+
+    String leader = "";
+
+    ArrayList<Car> cars = new ArrayList<>();
+
+
+}
